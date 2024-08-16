99734771cba53b84d24034c0f9c183619f236fd3
GoksuKO <aysegoksu2023@gmail.com>
2024-08-16 14:53:17 +0200

    played a little

---
+++ is_palindrome.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Palindrome: "Was it a car or a cat I saw?????", "noon", "Dad"
+// enhanced
+
+bool is_not_letter(char x)
+{
+  if (x >= 'A' && x <= 'Z')
+    return false;
+  else if (x >= 'a' && x <= 'z')
+    return false;
+  else
+    return true;
+}
+
+bool is_capital(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return true;
+	else
+		return false;
+}
+
+bool is_palindrome(char *str)
+{
+  int i;
+  int j;
+  int len;
+  bool is_first;
+  
+  i = 0;
+  len = strlen(str);
+  j = len - 1;
+  is_first = true;
+  if (str == NULL)
+    return true;
+  while(j - i >= 2 && str[i] != '\0' && str[j] != '\0')
+  {
+    while (is_not_letter(str[i]) && str[i] != '\0')
+      i++;
+    while (is_not_letter(str[j]) && str[j] != '\0')
+      j--;
+    if (is_capital(str[i]))
+      str[i] += 32;
+    if (is_capital(str[j]))
+      str[j] += 32;
+    if (i == len && is_first)
+      return true;      
+    if (str[i] != str[j])
+      return false;
+    else
+    {
+      i++;
+      j--;
+    }
+    is_first = false;
+  }
+  return true;
+}
+
+int main(void) {
+  bool answer;
+  char *str;
+  
+  str = " at au  ";  
+  answer = is_palindrome(str);
+  
+  if (answer == true) 
+    printf("It's a palindrome.\n");
+  else
+    printf("It's not a palindrome.\n");
+  return 0;
+}
+
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <string.h>
+
+// // Palindrome: "Was it a car or a cat I saw?????", "noon", "Dad"
+// // my_solution
+
+// bool is_palindrome(char *str)
+// {
+//   int i;
+//   int j;
+//   int len;
+//   bool is_first;
+  
+//   i = 0;
+//   len = strlen(str);
+//   j = len - 1;
+//   is_first = true;
+//   if (str == NULL)
+//     return true;
+//   while(j - i >= 2 && str[i] != '\0' && str[j] != '\0')
+//   {
+//     while ((str[i] < 'A' || str[i] > 'Z') && str[i] != '\0')
+//       i++;
+//     while ((str[i] < 'a' || str[i] > 'z') && str[i] != '\0')
+//       i++;
+//     while ((str[j] < 'A' || str[j] > 'Z') && str[j] != '\0')
+//       j--;
+//     while ((str[j] < 'a' || str[j] > 'z') && str[j] != '\0')
+//       j--;
+//     if (str[i] >= 'A' && str[i] <= 'Z')
+//       str[i] += 32;
+//     if (str[j] >= 'A' && str[j] <= 'Z')
+//       str[j] += 32;
+//     if (i == len && is_first)
+//       return true;      
+//     if (str[i] != str[j])
+//       return false;
+//     else
+//     {
+//       i++;
+//       j--;
+//     }
+//     is_first = false;
+//   }
+//   return true;
+// }
+
+// int main(void) {
+//   bool answer;
+//   char *str;
+  
+//   str = "    ";  
+//   answer = is_palindrome(str);
+  
+//   if (answer == true) 
+//     printf("It's a palindrome.\n");
+//   else
+//     printf("It's not a palindrome.\n");
+//   return 0;
+// }
--- others/search_insert.c
+++ others/search_insert.c
@@ -1,13 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   my_exams.c                                         :+:    :+:            */
+/*   search_insert.c                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: akaya-oz <akaya-oz@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/05/11 22:48:19 by akaya-oz      #+#    #+#                 */
-/*   Updated: 2024/05/14 14:59:38 by akaya-oz      ########   odam.nl         */
+/*   Created: 2024/08/09 13:17:17 by akaya-oz      #+#    #+#                 */
+/*   Updated: 2024/08/09 13:17:20 by akaya-oz      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Rank2 1. ft_strcpy 2. rotone 3. union 4. ft_range
+#include <unistd.h>
+#include <stdio.h>
+
+int searchInsert(int *nums, int numsSize, int target)
+{
+	int i;
+
+	i = 0;
+	while (target > nums[i] && i < numsSize - 1)
+	{
+		// printf("i: %d, nums[%d]: %d\n", i , i , nums[i]);
+		i++;
+	}
+	if (i == numsSize - 1 && target > nums[i])
+		i += 1;
+	return (i);
+}
+
+// int main()
+// {
+// 	int nums[] = {1,3,5,6};
+
+// 	searchInsert(nums, 4, 5);
+// 	return 0;
+// }
