2e55e6f21bde87395a3a14c285016cdd713775dd
goksu <akaya-oz@student.codam.nl>
2024-09-13 14:21:28 +0200

    noted down my exam questions

--- my_exams.c
+++ my_exams.c
@@ -6,8 +6,9 @@
 /*   By: akaya-oz <akaya-oz@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/11 22:48:19 by akaya-oz      #+#    #+#                 */
-/*   Updated: 2024/05/14 14:59:38 by akaya-oz      ########   odam.nl         */
+/*   Updated: 2024/09/13 14:21:10 by akaya-oz      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Rank2 1. ft_strcpy 2. rotone 3. union 4. ft_range
+// Rank3 1. ft_split 2. ft_itoa 3. sort_int_tab
